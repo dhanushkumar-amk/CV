a1c958fba5eb4f3ef9d79b1066c885aefb076ad0
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-02-28 18:53:19 +0530

    uploaded2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,47 @@
-/*==================== SHOW MENU ====================*/
+/*==================== SHOW MENU ====================*/const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-
+  // Validate that variables exist
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      // We add the show-menu class to the div tag with the nav__menu class
+      nav.classList.toggle('show-menu');
+    });
+  }
+};
+showMenu('nav-toggle', 'nav-menu');
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== SHOW SCROLL TOP ====================*/
 
-/*==================== SHOW SCROLL TOP ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
 
+/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
 
-
-/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/ 
-
-
-/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/ 
-
-
-/*==================== GENERATE PDF ====================*/ 
+/*==================== GENERATE PDF ====================*/
 // PDF generated area
-
 
 // Html2pdf options
 
-
-// Function to call areaCv and Html2Pdf options 
-
+// Function to call areaCv and Html2Pdf options
 
 // When the button is clicked, it executes the three functions
 
-    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+// 1. The class .scale-cv is added to the body, where it reduces the size of the elements
 
+// 2. The PDF is generated
 
-    // 2. The PDF is generated
-
-
-    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+// 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
