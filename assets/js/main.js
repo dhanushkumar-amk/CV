1a297ca8891f6e09fb875fd503137226f5c9438b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-02-29 20:11:06 +0530

    uploaded3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
-/*==================== SHOW MENU ====================*/const showMenu = (toggleId, navId) => {
+/*==================== SHOW MENU ====================*/ const showMenu = (
+  toggleId,
+  navId
+) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
 
@@ -22,6 +25,37 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById('header');
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 200) nav.classList.add('scroll-header');
+  else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
 
 /*==================== SHOW SCROLL TOP ====================*/
 
